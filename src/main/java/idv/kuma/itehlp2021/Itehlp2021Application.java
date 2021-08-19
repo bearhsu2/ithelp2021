acba5e9cb65ed399595537545c143f34cfc7956c
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-19 18:08:19 +0800

    init

---
+++ src/main/java/idv/kuma/itehlp2021/Itehlp2021Application.java
@@ -0,0 +1,13 @@
+package idv.kuma.itehlp2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itehlp2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itehlp2021Application.class, args);
+    }
+
+}
---
+++ src/test/java/idv/kuma/itehlp2021/Itehlp2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.kuma.itehlp2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itehlp2021ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
