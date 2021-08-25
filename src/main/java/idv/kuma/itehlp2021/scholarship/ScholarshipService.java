8409824b6de0ac0660cb88be47cacc15d988b74f
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-25 11:13:38 +0800

    all test cases

--- src/main/java/idv/kuma/itehlp2021/scholarship/ScholarshipService.java
+++ src/main/java/idv/kuma/itehlp2021/scholarship/ScholarshipService.java
@@ -4,30 +4,26 @@ import java.util.List;
 
 public class ScholarshipService {
 
-
     public int calculate(Transcript transcript) {
 
         List<Course> courses = transcript.getCourses();
         if (courses.isEmpty()) return 0; // 不修課跟人家領什麼獎學金！
 
         int total = courses.size();
-
         int achieved = 0;
         for (Course course : courses) {
             if (course.getScore() >= 80) {
                 achieved++;
             }
         }
-
         double rate = (double) achieved / total;
 
-        if (rate > (double) 1 / 2) {
+        if (rate >= (double) 1 / 2) {
             return 10_000;
-        } else if (rate > (double) 1 / 3) {
+        } else if (rate >= (double) 1 / 3) {
             return 5_000;
         } else {
             return 0;
         }
-
     }
 }
--- src/test/java/idv/kuma/itehlp2021/scholarship/ScholarshipServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/ScholarshipServiceTest.java
@@ -18,7 +18,45 @@ class ScholarshipServiceTest {
         ));
 
         Assertions.assertEquals(10_000, actual);
+    }
 
+    @Test
+    void half_scholarship() {
 
+        ScholarshipService service = new ScholarshipService();
+
+        int actual = service.calculate(new Transcript(
+                new Course("Algorithm", 70),
+                new Course("Computer Internet", 70),
+                new Course("Operating System", 90)
+
+        ));
+
+        Assertions.assertEquals(5_000, actual);
+    }
+
+    @Test
+    void NO_courses() {
+
+        ScholarshipService service = new ScholarshipService();
+
+        int actual = service.calculate(new Transcript());
+
+        Assertions.assertEquals(0, actual);
+    }
+
+    @Test
+    void NO_scholarship() {
+
+        ScholarshipService service = new ScholarshipService();
+
+        int actual = service.calculate(new Transcript(
+                new Course("Algorithm", 70),
+                new Course("Computer Internet", 70),
+                new Course("Operating System", 70)
+
+        ));
+
+        Assertions.assertEquals(0, actual);
     }
 }
