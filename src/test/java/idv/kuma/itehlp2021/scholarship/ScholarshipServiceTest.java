763ea13b2bc223f7453ad98a1a260dc2ebc6b2a8
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-25 11:09:16 +0800

    refactor: inline temp variable

--- src/test/java/idv/kuma/itehlp2021/scholarship/ScholarshipServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/ScholarshipServiceTest.java
@@ -10,13 +10,12 @@ class ScholarshipServiceTest {
 
         ScholarshipService service = new ScholarshipService();
 
-        Transcript transcript = new Transcript(
+        int actual = service.calculate(new Transcript(
                 new Course("Algorithm", 70),
                 new Course("Computer Internet", 80),
                 new Course("Operating System", 90)
 
-        );
-        int actual = service.calculate(transcript);
+        ));
 
         Assertions.assertEquals(10_000, actual);
 
