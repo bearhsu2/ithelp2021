bceda8320dc4a7a1d06a7a55ac6b717e4883c91c
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-26 17:39:28 +0800

    finish test

--- src/main/java/idv/kuma/itehlp2021/scholarship/apply/Application.java
+++ src/main/java/idv/kuma/itehlp2021/scholarship/apply/Application.java
@@ -1,8 +1,9 @@
 package idv.kuma.itehlp2021.scholarship.apply;
 
-import idv.kuma.itehlp2021.scholarship.ScholarshipService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class Application {
     private long scholarshipId;
--- src/test/java/idv/kuma/itehlp2021/scholarship/apply/CheckApplicationServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/apply/CheckApplicationServiceTest.java
@@ -1,9 +1,12 @@
 package idv.kuma.itehlp2021.scholarship.apply;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 import java.time.LocalDate;
+
 
 class CheckApplicationServiceTest {
     @Test
@@ -11,6 +14,23 @@ class CheckApplicationServiceTest {
 
         ScholarshipRepository fakeRepository = Mockito.mock(ScholarshipRepository.class);
         Mockito.when(fakeRepository.find(777L)).thenReturn(new Scholarship(LocalDate.MAX));
+
+
+        LocalDate expected = LocalDate.of(2029, 12, 31);
+
+        MockedStatic<LocalDate> mockedStatic = Mockito.mockStatic(LocalDate.class);
+        mockedStatic.when(LocalDate::now).thenReturn(
+              expected
+        );
+
+
+        CheckApplicationService service = new CheckApplicationService(fakeRepository);
+
+        Application application = new Application(777L);
+
+        boolean actual = service.checkTime(application);
+
+        Assertions.assertTrue(actual);
 
 
 
