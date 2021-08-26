3091eb02ec78da1685ad0bc394d041e748db51f8
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-26 23:04:58 +0800

    add comment

--- src/test/java/idv/kuma/itehlp2021/scholarship/apply/ApplyScholarshipServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/apply/ApplyScholarshipServiceTest.java
@@ -1,7 +1,6 @@
 package idv.kuma.itehlp2021.scholarship.apply;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
@@ -9,17 +8,20 @@ class ApplyScholarshipServiceTest {
     @Test
     void check_ok_then_create() {
 
+        // 準備申請表
         Application application = new Application(777L);
 
+        // 準備假 checker
         CheckApplicationService checker = mock(CheckApplicationService.class);
         when(checker.checkTime(application)).thenReturn(true);
 
+        // 準備假 repository
         ApplicationRepository repository = mock(ApplicationRepository.class);
 
+        // 執行
         new ApplyScholarshipService(checker, repository).apply(application);
 
+        // 驗證：真的有 create 一次
         verify(repository, times(1)).create(application);
-
-
     }
 }
