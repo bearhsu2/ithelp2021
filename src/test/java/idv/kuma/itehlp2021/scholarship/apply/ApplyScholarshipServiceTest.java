09b735079050638b744b76cdef264c3dba371fa8
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-26 23:12:53 +0800

    add test case for "fail the check"

--- src/test/java/idv/kuma/itehlp2021/scholarship/apply/ApplyScholarshipServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/apply/ApplyScholarshipServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.*;
 
 class ApplyScholarshipServiceTest {
+
     @Test
     void check_ok_then_create() {
 
@@ -23,5 +24,24 @@ class ApplyScholarshipServiceTest {
 
         // 驗證：真的有 create 一次
         verify(repository, times(1)).create(application);
+    }
+    @Test
+    void check_NOT_ok_then_DONT_create() {
+
+        // 準備申請表
+        Application application = new Application(777L);
+
+        // 準備假 checker
+        CheckApplicationService checker = mock(CheckApplicationService.class);
+        when(checker.checkTime(application)).thenReturn(false);
+
+        // 準備假 repository
+        ApplicationRepository repository = mock(ApplicationRepository.class);
+
+        // 執行
+        new ApplyScholarshipService(checker, repository).apply(application);
+
+        // 驗證：真的完全有 create 過
+        verify(repository, never()).create(application);
     }
 }
