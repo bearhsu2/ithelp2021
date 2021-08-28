a5417e51551e9c260aec34c47a2a21b54cccd78a
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-29 00:02:39 +0800

    refactor test

--- src/test/java/idv/kuma/itehlp2021/scholarship/register/RegisterServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/register/RegisterServiceTest.java
@@ -1,30 +1,37 @@
 package idv.kuma.itehlp2021.scholarship.register;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Fail.fail;
 
 class RegisterServiceTest {
 
+    private final StudentRepository repository = Mockito.mock(StudentRepository.class);
+
     @Test
     void when_student_not_exists() throws DataNotFoundException {
 
-        StudentRepository repository = Mockito.mock(StudentRepository.class);
-        Mockito.doThrow(new DataNotFoundException())
-                .when(repository).register(request(35L));
-
-        RegisterService service = new RegisterService(repository);
+        given_student_NOT_exists(35L);
 
         try {
-            service.execute(request(35L));
+            create_register_service().execute(request(35L));
             fail("should throw exception");
         } catch (StudentNotExistException e) {
-            Assertions.assertThat(e).hasMessageContaining("not exists");
+            assertThat(e).hasMessageContaining("not exists");
         }
 
 
+    }
+
+    private RegisterService create_register_service() {
+        return new RegisterService(repository);
+    }
+
+    private void given_student_NOT_exists(long studentId) throws DataNotFoundException {
+        Mockito.doThrow(new DataNotFoundException())
+                .when(repository).register(request(studentId));
     }
 
     private RegisterRequest request(long studentId) {
