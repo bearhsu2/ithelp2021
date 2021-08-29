ebefd77e9b25cee40543a48190ece12d527ccc1c
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-29 14:44:57 +0800

    enable tests for better protection

--- src/test/java/idv/kuma/itehlp2021/scholarship/register/RegisterControllerTest.java
+++ src/test/java/idv/kuma/itehlp2021/scholarship/register/RegisterControllerTest.java
@@ -2,7 +2,6 @@ package idv.kuma.itehlp2021.scholarship.register;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled
 @SpringBootTest
 @AutoConfigureMockMvc
 class RegisterControllerTest {
