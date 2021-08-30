e19472916603b5f81a1df24a19d6879efc3a5634
Yu-Song Syu <kuma@cloud-latitude.com>
2021-08-30 18:24:21 +0800

    refactor: extract methods for scholarship calculation

--- src/main/java/idv/kuma/itehlp2021/scholarship/ScholarshipService.java
+++ src/main/java/idv/kuma/itehlp2021/scholarship/ScholarshipService.java
@@ -9,54 +9,13 @@ public class ScholarshipService {
         String programType = transcript.getProgramType();
 
         if (programType.equals("Bachelor")) {
-
-            List<Course> courses = transcript.getCourses();
-            if (courses.isEmpty()) return 0; // 不修課跟人家領什麼獎學金！
-
-            int total = courses.size();
-            int achieved = 0;
-            for (Course course : courses) {
-                if (course.getScore() >= 80) {
-                    achieved++;
-                }
-            }
-            double rate = (double) achieved / total;
-
-            if (rate >= (double) 1 / 2) {
-                return 10_000;
-            } else if (rate >= (double) 1 / 3) {
-                return 5_000;
-            } else {
-                return 0;
-            }
+            return calculateBachelor(transcript);
         }
-
 
         if (programType.equals("Master")) {
 
-            List<Course> courses = transcript.getCourses();
-            if (courses.isEmpty()) return 0; // 不修課跟人家領什麼獎學金！
-
-            double totalCredit = 0.001D;
-            double totalWeightedScore = 0D;
-
-            for (Course course : courses) {
-                totalCredit += course.getCredit();
-                totalWeightedScore += course.getScore() * course.getCredit();
-            }
-
-            double weightedAverage = totalWeightedScore / totalCredit;
-
-
-            if (weightedAverage >= 90D) {
-                return 15_000;
-            } else if (weightedAverage >= 80D) {
-                return 7_500;
-            } else {
-                return 0;
-            }
+            return calculateMaster(transcript);
         }
-
 
         if (programType.equals("PhD")) {
 
@@ -76,6 +35,53 @@ public class ScholarshipService {
         }
 
         throw new UnknownProgramTypeException(programType);
+
+    }
+
+    private int calculateMaster(Transcript transcript) {
+        List<Course> courses = transcript.getCourses();
+        if (courses.isEmpty()) return 0; // 不修課跟人家領什麼獎學金！
+
+        double totalCredit = 0.001D;
+        double totalWeightedScore = 0D;
+
+        for (Course course : courses) {
+            totalCredit += course.getCredit();
+            totalWeightedScore += course.getScore() * course.getCredit();
+        }
+
+        double weightedAverage = totalWeightedScore / totalCredit;
+
+
+        if (weightedAverage >= 90D) {
+            return 15_000;
+        } else if (weightedAverage >= 80D) {
+            return 7_500;
+        } else {
+            return 0;
+        }
+    }
+
+    private int calculateBachelor(Transcript transcript) {
+        List<Course> courses = transcript.getCourses();
+        if (courses.isEmpty()) return 0; // 不修課跟人家領什麼獎學金！
+
+        int total = courses.size();
+        int achieved = 0;
+        for (Course course : courses) {
+            if (course.getScore() >= 80) {
+                achieved++;
+            }
+        }
+        double rate = (double) achieved / total;
+
+        if (rate >= (double) 1 / 2) {
+            return 10_000;
+        } else if (rate >= (double) 1 / 3) {
+            return 5_000;
+        } else {
+            return 0;
+        }
     }
 
     public static class UnknownProgramTypeException extends Throwable {
