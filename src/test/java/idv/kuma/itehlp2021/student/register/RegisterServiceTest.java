f570f863b3b14023de197360bc5f6b5337933dd3
Yu-Song Syu <kuma@cloud-latitude.com>
2021-09-09 13:05:13 +0800

    add alternative exception testing methods

--- src/test/java/idv/kuma/itehlp2021/student/register/RegisterServiceTest.java
+++ src/test/java/idv/kuma/itehlp2021/student/register/RegisterServiceTest.java
@@ -1,5 +1,6 @@
 package idv.kuma.itehlp2021.student.register;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -21,6 +22,20 @@ class RegisterServiceTest {
         } catch (StudentNotExistException e) {
             assertThat(e).hasMessageContaining("not exists");
         }
+
+
+    }
+
+    @Test
+    void when_student_not_exists_alternative() throws DataNotFoundException {
+
+        given_student_NOT_exists(35L);
+
+        StudentNotExistException actualException = Assertions.assertThrows(
+                StudentNotExistException.class,
+                () -> create_register_service().execute(request(35L))
+        );
+        assertThat(actualException).hasMessageContaining("not exists");
 
 
     }
